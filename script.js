88c6ee2ed6eb194c294b061ea470e4e19983aa41
dubianliao <dubianliao@gmail.com>
2019-02-08 00:33:36 +0900

    k

--- script.js
+++ script.js
@@ -7,8 +7,19 @@
 
 $(function(){
 
-
-		
+	$('.list-item').click(function(){
+		var $mark = $(this).find('span');
+		var $kotae = $(this).find('.kotae');
+		if($kotae.hasClass('open')){
+			$kotae.removeClass('open');
+			$kotae.slideUp();
+			$mark.html('+');
+		} else {
+			$kotae.addClass('open');
+			$kotae.slideDown();
+			$mark.html('-');
+		}
+	});
 
 
 });//全体の閉じタグ
